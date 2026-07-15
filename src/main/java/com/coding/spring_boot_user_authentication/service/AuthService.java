6757ad61efc1bc4b99b8743a0a373d484b52c9fd
pavan0209 <pavanasonawane@gmail.com>
2026-07-15 23:41:49 +0530

    added AuthService interface

---
+++ src/main/java/com/coding/spring_boot_user_authentication/service/AuthService.java
@@ -0,0 +1,14 @@
+package com.coding.spring_boot_user_authentication.service;
+
+import com.coding.spring_boot_user_authentication.dto.request.LoginRequest;
+import com.coding.spring_boot_user_authentication.dto.request.RegisterRequest;
+import com.coding.spring_boot_user_authentication.dto.response.LoginResponse;
+import com.coding.spring_boot_user_authentication.dto.response.UserResponse;
+
+public interface AuthService {
+
+    UserResponse registerUser(RegisterRequest request);
+
+    LoginResponse loginUser(LoginRequest request);
+
+}
