d8e764ac77feaf61ed1ed837470a048671170eb1
pavan0209 <pavanasonawane@gmail.com>
2026-07-15 22:36:41 +0530

    initial commit

---
+++ src/main/java/com/coding/spring_boot_user_authentication/SpringBootUserAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.coding.spring_boot_user_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUserAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUserAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coding/spring_boot_user_authentication/SpringBootUserAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.spring_boot_user_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUserAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
