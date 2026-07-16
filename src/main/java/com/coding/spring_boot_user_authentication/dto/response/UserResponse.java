af85bda5c0e2fd07d0cc851b5704bf907a7c677e
pavan0209 <pavanasonawane@gmail.com>
2026-07-16 22:03:15 +0530

    added validation dependency and UserResponse DTO

---
+++ src/main/java/com/coding/spring_boot_user_authentication/dto/response/UserResponse.java
@@ -0,0 +1,21 @@
+package com.coding.spring_boot_user_authentication.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class UserResponse {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String phoneNumber;
+}
